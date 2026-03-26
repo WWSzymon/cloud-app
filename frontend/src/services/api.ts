0c5eb909714d5dc6707a06094c7c4b93da075af0
Szymon Wójcik <szymonwojcik@MacBook-Air-Szymon-2.local>
2026-03-26 23:57:08 +0100

    Artefakt 05 - Stabilność i Trwałość ukończone

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  // Dodajemy "/api" do adresu pobranego ze zmiennej środowiskowej
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 export default api;
